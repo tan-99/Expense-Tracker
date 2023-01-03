651a4ee03235401cde560f3f2810a60434c4201b
Tanishqa Sawant <102019409+tan-99@users.noreply.github.com>
2023-01-03 23:58:06 +0530

    js-variables added

---
+++ expense.js
@@ -0,0 +1,9 @@
+const balance = document.getElementById('balance');
+
+const incAmt = document.getElementById('inc-amt');
+const expAmt = document.getElementById('exp-amt');
+
+const list = document.getElementById('list');
+
+const transDetails = document.getElementById('transac-details');
+const amount = document.getElementById('amount');
