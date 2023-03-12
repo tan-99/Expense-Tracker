d3a115528d215070c857131e8c77214ee7219248
Tanishqa Sawant <102019409+tan-99@users.noreply.github.com>
2023-03-12 14:41:39 +0530

    js ready

--- expense.js
+++ expense.js
@@ -1,101 +1,207 @@
-var balance = document.getElementById('balance');
+var state = {
+    balance: 0,
+    earning: 0,
+    expense:  0,
+    transaction: [],
+    // transaction: [{details: 'Salary', amount: 2400, type: 'earning'},
+    //               {details: 'Grocery', amount: 350, type: 'expense'},
+    //               {details: 'Bonus', amount: 1300, type: 'earning'},
+    //               {details: 'Furniture', amount: 560, type: 'expense'},
+    //               {details: 'Eating out', amount: 89, type: 'expense'} 
+};
 
-var earnAmt = document.getElementById('earn-amt');
+var balanceEl = document.getElementById('balance');
+var earningEl = document.getElementById('earn-amt');
+var expenseEl = document.getElementById('exp-amt');
+var transactionEl = document.getElementById('list'); 
+var earningBtnEl = document.getElementById('transac-earning-btn');
+var expenseBtnEl = document.getElementById('transac-expense-btn');
+var nameInputEl = document.getElementById('transac-details');
+var amountInputEl = document.getElementById('amount'); 
 
-var expAmt = document.getElementById('exp-amt');
+//id generator function
+function uniqueId(){
+    return Math.round(Math.random()*100000);
+}
 
-const histList = document.getElementById('list');
+//initialising function
+function init(){
 
+    var localState = JSON.parse(localStorage.getItem('expenseTrackerState'));
 
-const addTransactionsButton = document.getElementById('transac-btn');
+    if(localState !== null){
+        state = localState;
+    }
+    
+    updateState();
+    initListeners();
+}
 
+init();
 
+//function used for state manipulation
+function updateState(){
+    //only for state manipulation
+    var balance = 0,
+        earning = 0,
+        expense = 0,
+        item;
 
-/*addTransactionsButton.addEventListener('click',function(){
+        for(var i = 0; i < state.transaction.length; i++){
 
-});*/
+            item = state.transaction[i];
+            if(item.type == 'earning'){
+                earning += item.amount;
+                balance += item.amount;
+            }
+            else if(item.type == 'expense'){
+                expense += item.amount;
+                balance -= item.amount;
+            }
+    
+    
+            console.log(item);
+            state.balance = balance;
+            state.earning = earning;
+            state.expense = expense;
+    
+        }
 
-const transDetails = document.getElementById('transac-details');
-const amount = document.getElementById('amount');
-var strBal = balance.innerText;
-var strEarn = earnAmt.innerText;
-var strExp = expAmt.innerText;
+        if(state.transaction.length === 0){
+            state.balance = 0;
+            state.earning = 0;
+            state.expense = 0;
+        }
+        
+        
+        localStorage.setItem('expenseTrackerState', JSON.stringify(state));
 
-addTransactionsButton.addEventListener('click', function(){
+    render();
+}
+
+//function to listen for events
+function initListeners(){
+
+    earningBtnEl.addEventListener('click',onAddEarningClick);
+    expenseBtnEl.addEventListener('click',onAddExpenseClick);    
+}
+
+function onAddEarningClick(){
+
+    addTransaction(nameInputEl.value, amount.value, 'earning');    
+}
+
+function onAddExpenseClick(){
+    
+    addTransaction(nameInputEl.value, amount.value, 'expense');
+}
+
+function addTransaction(details, amount, type){
+
+    var id = uniqueId();
+
+    //if none has been entered
+
+    if(amount == "" && details == ""){
+        alert("Enter transaction details and amount !");
+    } 
 
     //if only amount has been entered
 
-    if(amount.value != "" && transDetails.value == ""){
+    else if(amount != "" && details == ""){
         alert("Enter transaction details!");
     }
     
     //if only transaction-details have been entered
 
-    else if(amount.value=="" && transDetails.value != ""){
+    else if(amount =="" && details != ""){
         alert("Enter amount!");
     }
 
-    //if both - transaction-details and amount is entered
+    else{
+        var transaction = { 
+            id: id,
+            details: details, 
+            amount: parseInt(amount), 
+            type: type,}
+    
+        /*typeof(amountInputEl.value)
+        'string' */
+    
+        state.transaction.push(transaction);
+        updateState();
+    
+        nameInputEl.value = '';
+        amountInputEl.value = "";
+    }
 
-    else if(amount.value != "" && transDetails.value != ""){
+}
 
-        const sign = amount.value < 0 ? "-" : "+";
+function onClickDelete(event){
 
-        const item = document.createElement("li");
+    // console.log('why event not working')
+    console.log(event.target.parentNode);
+    var btnTarget = event.target.parentNode;
+    var id = parseInt(btnTarget.getAttribute('data-id')); //getAttribute returns a string
+    console.log(id);
 
-        item.classList.add(
-            (amount.value > 0) ? "plus" : "minus"
-        );
+    //The splice() method in JavaScript is used to add or remove elements from an array. 
+    //It can modify the original array and return the removed elements as a new array.
 
-        //updation of balance
+    var deleteIndex;
+    for(var i = 0; i < state.transaction.length; i++){
+        if(state.transaction[i].id === id){
+            deleteIndex = i;
+            break;
+        }
+    }
+
+    state.transaction.splice(deleteIndex, 1);
+
+    updateState();
+}
+
+function render(){
+
+    balanceEl.innerHTML = `$${state.balance}`;
+    earningEl.innerHTML = `$${state.earning}`;
+    expenseEl.innerHTML = `$${state.expense}`;
+
+    var listEl, item, btnEl;
+
+    transactionEl.innerHTML = '';
+
+    for(var i = 0; i < state.transaction.length; i++){
         
-        strBal = strBal.replace('$','');
-        strBal = (Number(strBal) + Number(amount.value));
-        strBal = strBal.toString();
+        item = state.transaction[i];
+        listEl = document.createElement('li');
 
-        balance.innerHTML = `<p class = "fs-1 fw-bold" id = "balance">$${strBal}</p>`;
-
-        //updation
-        
-        if(amount.value > 0){
-
-            strEarn = strEarn.replace('+$','');
-            strEarn = (Number(strEarn) + Number(amount.value));
-            strEarn = strEarn.toString();
-
-            earnAmt.innerHTML = `<h4 id = "earn-amt" class = "earn-amt text-center fw-bold">+$${strEarn}</h4>`;
+        if(item.type=='earning'){
+            listEl.classList.add("hist-list","plus");
         }
         else{
+            listEl.classList.add("hist-list","minus");
+        } 
 
-            strExp = strExp.replace('-$','');
-            strExp = (Number(strExp) + Math.abs(Number(amount.value)));
-            strExp = strExp.toString();
+        listEl.innerHTML=`${item.details}<span>$${item.amount}</span>`;
+        
 
-            expAmt.innerHTML = `<h4 id = "exp-amt" class = "exp-amt text-center fw-bold">-$${strExp}</h4>`;
+        btnEl = document.createElement('button');
+        btnEl.className="delete-btn";
 
-        }
+        btnEl.innerHTML=`<i class="fa fa-close"></i>`;
+        btnEl.setAttribute('data-id',item.id);
+        
+        btnEl.style.cssText="font-size:17px; width: 25px; ";
+        btnEl.addEventListener('click', onClickDelete);
+        
 
 
+        listEl.appendChild(btnEl);
 
-        //updation of history
+        transactionEl.append(listEl);
+        console.log("why");
 
-        item.innerHTML = `
-            ${transDetails.value}<span>${sign}${Math.abs(amount.value)}</span>
-            <button class="delete-btn" style = "font-size:17px; width: 25px; ">
-                                <i class="fa fa-close"></i>
-                            </button>`;
-
-        histList.appendChild(item);
-
-        transDetails.value = "";
-        amount.value = "";
     }
 
-    //No input fields are  filled
-
-    else{
-        alert('Fill in the required details!');
-    }
-
-    
-});
+}
---
+++ expenseRef.js
@@ -0,0 +1,108 @@
+var balance = document.getElementById('balance');
+
+var earnAmt = document.getElementById('earn-amt');
+
+var expAmt = document.getElementById('exp-amt');
+
+const histList = document.getElementById('list');
+
+
+const addTransactionsButton = document.getElementById('transac-btn');
+
+const deleteBtn = document.getElementById('delete-btn');
+
+
+
+/*addTransactionsButton.addEventListener('click',function(){
+
+});*/
+
+const transDetails = document.getElementById('transac-details');
+const amount = document.getElementById('amount');
+var strBal = balance.innerText;
+var strEarn = earnAmt.innerText;
+var strExp = expAmt.innerText;
+
+addTransactionsButton.addEventListener('click', function(){
+
+    //if only amount has been entered
+
+    if(amount.value != "" && transDetails.value == ""){
+        alert("Enter transaction details!");
+    }
+    
+    //if only transaction-details have been entered
+
+    else if(amount.value=="" && transDetails.value != ""){
+        alert("Enter amount!");
+    }
+
+    //if both - transaction-details and amount is entered
+
+    else if(amount.value != "" && transDetails.value != ""){
+
+        const sign = amount.value < 0 ? "-" : "+";
+
+        const item = document.createElement("li");
+
+        item.classList.add(
+            (amount.value > 0) ? "plus" : "minus"
+        );
+
+        //updation of balance
+        
+        strBal = strBal.replace('$','');
+        strBal = (Number(strBal) + Number(amount.value));
+        strBal = strBal.toString();
+
+        balance.innerHTML = `<p class = "fs-1 fw-bold" id = "balance">$${strBal}</p>`;
+
+        //updation
+        
+        if(amount.value > 0){
+
+            strEarn = strEarn.replace('+$','');
+            strEarn = (Number(strEarn) + Number(amount.value));
+            strEarn = strEarn.toString();
+
+            earnAmt.innerHTML = `<h4 id = "earn-amt" class = "earn-amt text-center fw-bold">+$${strEarn}</h4>`;
+        }
+        else{
+
+            strExp = strExp.replace('-$','');
+            strExp = (Number(strExp) + Math.abs(Number(amount.value)));
+            strExp = strExp.toString();
+
+            expAmt.innerHTML = `<h4 id = "exp-amt" class = "exp-amt text-center fw-bold">-$${strExp}</h4>`;
+
+        }
+
+
+
+        //updation of history
+
+        item.innerHTML = `
+            ${transDetails.value}<span>${sign}${Math.abs(amount.value)}</span>
+            <button class="delete-btn" style = "font-size:17px; width: 25px; ">
+                                <i class="fa fa-close"></i>
+                            </button>`;
+
+        histList.appendChild(item);
+
+        transDetails.value = "";
+        amount.value = "";
+    }
+
+    //No input fields are  filled
+
+    else{
+        alert('Fill in the required details!');
+    }
+
+    
+});
+
+deleteBtn.addEventListener('click',function(e){
+
+   
+});
